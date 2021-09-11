d3e5bf0971c2059a45cc3d6a3c8d8259707d6c17
ReEngine <amelin.it@gmail.com>
2021-09-11 19:51:53 +0300

    Add subtraction tests for ints

---
+++ CourseApp.Tests/SubTests.cs
@@ -0,0 +1,62 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class SubTests
+    {
+        [Fact]
+        public void SubIntsNegNegPos()
+        {
+            int a = -3;
+            int b = -4;
+            int expected = 1;
+
+            int actual = new Calculator().Sub(a, b);
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void SubIntsNegPosNeg()
+        {
+            int a = -3;
+            int b = 2;
+            int expected = -5;
+
+            int actual = new Calculator().Sub(a, b);
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void SubIntsPosNegPos()
+        {
+            int a = 5;
+            int b = -4;
+            int expected = 9;
+
+            int actual = new Calculator().Sub(a, b);
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void SubIntsPosPosPos()
+        {
+            int a = 3;
+            int b = 2;
+            int expected = 1;
+
+            int actual = new Calculator().Sub(a, b);
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void SubIntsNulNulNul()
+        {
+            int a = 0;
+            int b = 0;
+            int expected = 0;
+
+            int actual = new Calculator().Sub(a, b);
+            Assert.Equal(actual, expected);
+        }
+    }
+}
