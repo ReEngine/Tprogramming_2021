2bf6f62014eae495012cad68ea819c17f1432499
Игорь Амелин <amelin.it@gmail.com>
2021-09-23 14:47:46 +0300

    Add class to calculate hard-coded function from 1st variant and starting it in Main

---
+++ CourseApp/FunctionCalculator.cs
@@ -0,0 +1,35 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class FunctionCalculator
+    {
+        public void Calculate(double b, double x)
+        {
+            double top = 1 + Math.Pow(
+                Math.Sin(Math.Pow(b, 3) + Math.Pow(x, 3)),
+                2);
+            double bottom = Math.Pow(
+                Math.Pow(b, 3) + Math.Pow(x, 3),
+                1 / 3);
+            double y = top / bottom;
+            Console.WriteLine(y);
+        }
+
+        public void CalculateTaskA(double xStart, double xEnd, double dX, double b)
+        {
+            for (double x = xStart; x < xEnd; x += dX)
+            {
+                Calculate(b, x);
+            }
+        }
+
+        public void CalculateTaskB(double[] nums, double b)
+        {
+            foreach (double number in nums)
+            {
+                Calculate(b, number);
+            }
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,8 +6,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Hello world");
-            Console.ReadLine();
+            const double b = 2.5;
+            var funcalc = new FunctionCalculator();
+            Console.WriteLine("Calculating task A\n");
+            funcalc.CalculateTaskA(1.28, 3.28, 0.4, b);
+            double[] nums = new double[] { 1.1, 2.4, 3.6, 1.7, 3.9 };
+            Console.WriteLine("Calculating task B\n");
+            funcalc.CalculateTaskB(nums, b);
+            Console.ReadKey();
         }
     }
 }
