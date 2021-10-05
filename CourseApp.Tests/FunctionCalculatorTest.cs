394eb66322c4129739772927d98fb5192f216277
Игорь Амелин <amelin.it@gmail.com>
2021-10-05 14:37:14 +0300

    Some fixes

---
+++ CourseApp.Tests/FunctionCalculatorTest.cs
@@ -0,0 +1,13 @@
+namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class FunctionCalculatorTest
+    {
+        [Fact]
+        public void TestArray()
+        {
+            Assert.True(true);
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -8,11 +8,9 @@
         {
             const double b = 2.5;
             var funcalc = new FunctionCalculator();
-            Console.WriteLine("Calculating task A\n");
-            funcalc.CalculateTaskA(1.28, 3.28, 0.4, b);
+            Console.WriteLine("Calculating task A\n" + funcalc.CalculateTaskA(1.28, 3.28, 0.4, b));
             double[] nums = new double[] { 1.1, 2.4, 3.6, 1.7, 3.9 };
-            Console.WriteLine("Calculating task B\n");
-            funcalc.CalculateTaskB(nums, b);
+            Console.WriteLine("Calculating task B\n" + funcalc.CalculateTaskB(nums, b));
             Console.ReadKey();
         }
     }
