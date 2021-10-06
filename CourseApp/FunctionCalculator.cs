6b99ca4ca46cbd7ad8eac22b27a5d435964a3fb7
Игорь Амелин <amelin.it@gmail.com>
2021-10-06 10:38:14 +0300

    Removed unneeded variable in return part in Calculate method of FunctionCalculator

--- CourseApp/FunctionCalculator.cs
+++ CourseApp/FunctionCalculator.cs
@@ -9,8 +9,7 @@
         {
             double top = 1 + Math.Pow(Math.Sin(Math.Pow(b, 3) + Math.Pow(x, 3)), 2);
             double bottom = Math.Pow(Math.Pow(b, 3) + Math.Pow(x, 3), 1 / 3);
-            double y = top / bottom;
-            return y;
+            return top / bottom;
         }
 
         public List<double> CalculateTaskA(double xStart, double xEnd, double dX, double b)
