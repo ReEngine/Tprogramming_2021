95c3af51eb6ba2a4a944c5c6b90b90f566d25fe5
Игорь Амелин <amelin.it@gmail.com>
2021-11-13 16:07:32 +0300

    Migrated to .NET 6.0, added InlineData

--- CourseApp.Tests/AddTests.cs
+++ CourseApp.Tests/AddTests.cs
@@ -4,101 +4,19 @@
 
     public class AddTests
     {
-        [Fact]
-        public void AddIntsNegNegNeg()
+        [Theory]
+        [InlineData(-3, -4, -7)]
+        [InlineData(-3, 2, -1)]
+        [InlineData(-3, 4, 1)]
+        [InlineData(4, -5, -1)]
+        [InlineData(5, -4, 1)]
+        [InlineData(3, 2, 5)]
+        [InlineData(-3, 3, 0)]
+        [InlineData(3, -3, 0)]
+        [InlineData(0, 0, 0)]
+
+        public void AddIntsInlineTheory(int a, int b, int expected)
         {
-            int a = -3;
-            int b = -4;
-            int expected = -7;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsNegPosNeg()
-        {
-            int a = -3;
-            int b = 2;
-            int expected = -1;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsNegPosPos()
-        {
-            int a = -3;
-            int b = 4;
-            int expected = 1;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsPosNegNeg()
-        {
-            int a = 4;
-            int b = -5;
-            int expected = -1;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsPosNegPos()
-        {
-            int a = 5;
-            int b = -4;
-            int expected = 1;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsPosPosPos()
-        {
-            int a = 3;
-            int b = 2;
-            int expected = 5;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsNegPosNul()
-        {
-            int a = -3;
-            int b = 3;
-            int expected = 0;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsPosNegNul()
-        {
-            int a = 3;
-            int b = -3;
-            int expected = 0;
-
-            int actual = new Calculator().Add(a, b);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddIntsNulNulNul()
-        {
-            int a = 0;
-            int b = 0;
-            int expected = 0;
-
             int actual = new Calculator().Add(a, b);
             Assert.Equal(expected, actual);
         }
